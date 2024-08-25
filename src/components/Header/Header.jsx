ba4490d1d0bbcca65917bd29a58801489b83cdc8
DIPESH15 <dipeshjain16475@gmail.com>
2024-08-25 16:39:19 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+import './Header.scss';
+
+const Header=()=>(
+    <div className="header">
+    <h2>Welcome My Friend</h2>
+    </div>
+);
+export default Header;
